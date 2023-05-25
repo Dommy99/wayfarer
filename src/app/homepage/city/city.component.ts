7ac54932a0835554ebd3c61b99b33bcd2a9ff993
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-25 08:55:02 -0700

    added json file and city servce

--- src/app/homepage/city/city.component.ts
+++ src/app/homepage/city/city.component.ts
@@ -1,31 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'; 
+import { CitydataService } from '../citydata.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-city',
   templateUrl: './city.component.html',
   styleUrls: ['./city.component.css']
 })
-export class CityComponent {
-  cities = [
-    {
-      name: 'London',
-      country: 'United Kingdom',
-      img: '../../../assets/images/london.jpg'
-    },
-    {
-      name: 'San Francisco',
-      country: 'United States',
-      img: '../../../assets/images/sanfran.jpg'
-    },
-    {
-      name: 'Sydney',
-      country: 'Australia',
-      img: '../../../assets/images/sydney.jpg'
-    },
-    {
-      name: 'Gibraltar',
-      country: 'United Kingdom',
-      img: '../../../assets/images/gibraltar.jpg'
-    }
-  ];
+export class CityComponent implements OnInit {
+  cityTitle: string = '';
+  cityCountry: string = '';
+  cityImg: string = '';
+  posts: Array<string> = [];
+  searchSubject = new Subject();
+  cities: any[] = [];
+
+  constructor(private citydata: CitydataService, private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.http.get('../../assets/cityData.json').subscribe(data => {
+      this.cities = data[cities];
+      // Now this.cities will have the data from your JSON file
+    });
+  }
 }
