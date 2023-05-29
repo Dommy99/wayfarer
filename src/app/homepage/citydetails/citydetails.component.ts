b3292d63549dc69f81204e6fb672bc55a08b5a63
rjsehrlich <racheljsehrlich@gmail.com>
2023-05-29 11:22:13 -0700

    added weather to citydetails

--- src/app/homepage/citydetails/citydetails.component.ts
+++ src/app/homepage/citydetails/citydetails.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import cityData from '../../../assets/cityData.json';
+import { HttpClient } from '@angular/common/http';
 
 interface City {
   id: string;
   name: string;
   country: string;
+  countryCode: string;
   img: string;
   posts: { id: string; title: string; content: string }[];
 }
@@ -13,6 +15,7 @@ interface City {
 interface CityData {
   cities: City[];
 }
+
 
 @Component({
   selector: 'app-citydetails',
@@ -22,13 +25,30 @@ interface CityData {
 export class CitydetailsComponent implements OnInit {
   cities: CityData = cityData;
   city?: City;
+  currentTemp: number | undefined;
+  currentWeatherDescription: string | undefined;
+  weatherData: any;
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       const cityId = params['id']; 
       this.city = this.cities.cities.find(city => city.id === cityId); // find city by id
+
+      if (this.city) {
+        this.getWeather(this.city.name, this.city.countryCode);
+      }
     });
+  }
+
+  // call api to get weather data
+  getWeather(city: String, countryCode: string) {
+    this.http.get(`https://api.openweathermap.org/data/2.5/weather?q=${city},${countryCode}&appid=052f26926ae9784c2d677ca7bc5dec98&&units=imperial`)
+    .subscribe(response => {
+            this.weatherData = response;
+            this.currentTemp = this.weatherData.main.temp;
+            this.currentWeatherDescription = this.weatherData.weather[0].description;
+          });
   }
 }
